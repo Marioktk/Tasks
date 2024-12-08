3efff6c2a431b947d1f181eb16067dceb419c8a8
Mario <rehmanumair1912@gmail.com>
2024-12-08 02:17:08 -0800

    uploading code

--- Task1.js
+++ Task1.js
@@ -1,14 +1,14 @@
 let num = prompt("Enter any number");
 console.log(`Multiplication table of: ${num}`);
 
-let primeCount = 0;  // To count total prime numbers
+let primeCount = 0;  
 
 
 for (let i = 1; i <= 10; i++) {
     let result = num * i;
     console.log(`${num} x ${i} = ${result}`);
 
-    // Check if the result is a prime number
+    
     let count = 0;
 
     
@@ -18,12 +18,12 @@ for (let i = 1; i <= 10; i++) {
         }
     }
 
-    // If count is more than 2, it's not a prime number
+   
     if (count === 2) {  
         console.log(`${result} is a prime number`);
         primeCount++; 
     }
 }
 
-// Show total prime numbers
+
 console.log(`Total prime numbers in the table: ${primeCount}`);
